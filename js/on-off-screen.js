759b295039f7fc6c22e3dc54a0104bd771fe7ca5
Kamil Ginatulin <kamil.gin@gmail.com>
2014-07-05 22:47:24 -0700

    attempts with colorbox

--- js/on-off-screen.js
+++ js/on-off-screen.js
@@ -1,19 +1,4 @@
-/*$(document).ready(function(){
-	$(document).scroll(function(){
-		console.log("On-Off Screen JQuery is loading up!");
-		var toggleView = $('.toggle-view');
-		if (toggleView.visible()) {
-			toggleView.addClass("on-screen").removeClass("off-screen");
-			console.log("toggleView is visible");
-		} else {
-			toggleView.removeClass("on-screen").addClass("off-screen");
-			console.log("toggleView is NOT visible");
-		}
-		}
-	)
-});
-*/
-
+/*
 $(document).ready(function(){
 	$(document).scroll(function(){
 		console.log("On-Off Screen JQuery is loading up!");
@@ -25,6 +10,24 @@ $(document).ready(function(){
 		} else {
 			toggleView.removeClass("on-screen").addClass("off-screen");
 			console.log("toggleView is NOT visible");
+		}
+		}
+	)
+});
+*/
+
+
+$(document).ready(function(){
+	$(window).scroll(function(){
+		console.log("On-Off Screen JQuery is loading up!");
+		var narrative = $('#narrative');
+		var toggleView = $('.toggle-view');
+		if (narrative.visible(true)) {
+			toggleView.removeClass("on-screen").addClass("off-screen");
+			console.log("narrative is visible");
+		} else {
+			toggleView.addClass("on-screen").removeClass("off-screen");
+			console.log("narrative is NOT visible");
 		}
 		}
 	)
