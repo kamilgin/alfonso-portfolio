e8b79e001608faa936f57ff2358c44b7cd128ece
Kamil Ginatulin <kamil.gin@gmail.com>
2014-07-12 16:40:26 -0700

    Redoing videos
    media queries to change video size

---
+++ project.js
@@ -0,0 +1,9 @@
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="1">
+            <File firstVisibleLine="46" xOffset="0" scrollWidth="93016" startPos="2993" endPos="2993" selMode="0" lang="HTML" encoding="-1" filename="C:\Users\Kamil\Dropbox\Kamil\Ecuad_Interaction_Design_Essentials\design_studio_1\alfonso-portfolio\index.html" />
+            <File firstVisibleLine="89" xOffset="0" scrollWidth="93016" startPos="1370" endPos="1370" selMode="0" lang="CSS" encoding="-1" filename="C:\Users\Kamil\Dropbox\Kamil\Ecuad_Interaction_Design_Essentials\design_studio_1\alfonso-portfolio\css\style.css" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
