a32cd06bfc0f79c3fef871ac027e934fc6d787de
Kamil Ginatulin <kamil.gin@gmail.com>
2014-07-13 18:19:43 -0700

    Added "fullscreen-sections.js" from http://jsfiddle.net/senff/WdF89/1/

---
+++ js/fullscreen-sections.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	console.log("fullscreen js is loading up");	
+	 vpw = $(window).width();
+	 vph = $(window).height();
+    $('.full-page').height(vph);
+    $('.full-page').width(vpw);
+})
+
+
