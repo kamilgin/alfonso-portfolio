ecab8805a870b41149f77a7e500464263c9f128f
Kamil Ginatulin <kamil.gin@gmail.com>
2014-07-19 19:33:33 -0700

    Fullscreen bg images

--- js/fullscreen-sections.js
+++ js/fullscreen-sections.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 	console.log("fullscreen js is loading up");	
+	//FROM http://jsfiddle.net/senff/WdF89/1/
+	vpw = $(window).width();
+	vph = $(window).height();
+    $('.full-height').height(vph);
+    $('.full-width').width(vpw);
+})
+
+$(window).resize(function(){
+	console.log("window has been resized");	
 	 vpw = $(window).width();
 	 vph = $(window).height();
-    $('.full-page').height(vph);
-    $('.full-page').width(vpw);
+    $('.full-height').height(vph);
+    $('.full-width').width(vpw);
 })
+
+
 
 
